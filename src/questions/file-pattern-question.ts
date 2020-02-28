92696c6925abc8c3482301406da4cc28466fec8b
iTeckHD <ituankha.tran+github@gmail.com>
2020-02-28 20:38:56 +0100

    feat: remove test pattern

--- src/questions/file-pattern-question.ts
+++ src/questions/file-pattern-question.ts
@@ -7,7 +7,6 @@ export function getFilePatternQuestion(): InputQuestion<
     type: "input",
     name: "result",
     message: `Enter the regex to match the files`,
-    default: "my-(\\S*)-(\\d*).mp4",
     filter: function(answer: string) {
       return new RegExp(answer);
     }
