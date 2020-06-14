ac45b10e964c060727c0c2faf4bab8f593db1469
Tuan Kha Tran <tran.tuankha@muffag.ch>
2020-06-14 18:43:53 +0200

    refactor: remove unnecessary code

--- src/util/print-transformation.ts
+++ src/util/print-transformation.ts
@@ -3,8 +3,6 @@ import { output } from "./output";
 export function printTransformation(
   files: Array<{ value: string; transformed: string }>
 ) {
-  const arrResult = [];
-
   for (const file of files) {
     output(file.value);
     output(`> ${file.transformed}`);
